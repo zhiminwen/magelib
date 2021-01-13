bf824d277f5e2f98032d78e695b075e063691fc2
Zhimin <wenzm@sg.ibm.com>
2021-01-13 23:23:35 +0800

    formating

--- ubuntu/docker.go
+++ ubuntu/docker.go
@@ -5,7 +5,7 @@ import "github.com/zhiminwen/quote"
 //Ubuntu 20.04
 func CmdToInstallDocker() string {
 	cmd := quote.CmdTemplate(`
-	sudo apt update
+		sudo apt update
 		sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 		sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
