e5963b9c5a895c3aae5f512bd37081527b0fda21
Zhimin <wenzm@sg.ibm.com>
2021-03-14 22:13:00 +0800

    update

---
+++ openshift/image.go
@@ -0,0 +1,79 @@
+package openshift
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/zhiminwen/quote"
+)
+
+type ImageData struct {
+	Reg    string
+	Repo   string
+	Digest string
+	Tag    string
+	Type   string
+}
+
+func CreateImgData(lists []string) []ImageData {
+	// List Sample:
+	// docker.io/ceph/ceph:v15.2.4
+	// 		Reg: docker.io
+	// 		Repo: /ceph/ceph
+	// 		tag: v15.2.4
+	result := []ImageData{}
+	for _, line := range lists {
+		data := strings.SplitN(line, "/", 2)
+		reg := data[0]
+		repoTags := strings.Split(data[1], ":")
+		repo, tag := repoTags[0], repoTags[1]
+
+		result = append(result, ImageData{
+			Reg:  reg,
+			Repo: repo,
+			Tag:  tag,
+		})
+	}
+
+	return result
+}
+
+type Repo struct {
+	Registry string
+	Repo     string
+}
+
+func getRepo(imgData []ImageData) map[string]Repo {
+	repoList := map[string]Repo{}
+	for _, img := range imgData {
+		key := fmt.Sprintf("%s/%s", img.Reg, filepath.Dir(img.Repo))
+		repoList[key] = Repo{
+			Registry: img.Reg,
+			Repo:     filepath.Dir(img.Repo),
+		}
+	}
+	return repoList
+}
+
+func GenOCPImagePolicyYaml(imgData []ImageData, name string, localRegUrl string) string {
+	repoList := getRepo()
+	content := quote.TemplateGeneric(quote.HereDoc(`
+    apiVersion: operator.openshift.io/v1alpha1
+    kind: ImageContentSourcePolicy
+    metadata:
+      name: {{ .name }}
+    spec:
+      repositoryDigestMirrors:
+      {{ range $key, $repo := .mirrors -}}
+      - mirrors:
+        - {{ $.localRegUrl }}/{{ $repo.Repo }}
+        source: {{ $key }}
+      {{ end }}
+  `), map[string]interface{}{
+		"mirrors":     repoList,
+		"localRegUrl": localRegUrl,
+	})
+
+	return content
+}
